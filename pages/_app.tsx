aada16259c729efffa8317ae7f69d7665e75f730
Ulternae <ulternae@gmail.com>
2025-11-19 15:07:31 -0500

    Scalable dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,20 @@ import { appWithTranslation } from 'next-i18next'
 import { useServerStyles } from '@ui/ssr'
 import { UIProvider } from '@ui/Provider'
 import { QueryProvider } from '@api/QueryProvider'
-import { Provider } from 'next-auth/client'
+import { SessionProvider } from '@auth/client'
 import '../ui/globals.css'
 
 const NextApp = ({ Component, pageProps }: AppProps) => {
   useServerStyles()
 
   return (
-    <Provider session={pageProps.session} >
+    <SessionProvider session={pageProps.session} >
       <QueryProvider>
         <UIProvider>
           <Component {...pageProps} />
         </UIProvider>
       </QueryProvider>
-    </Provider>
+    </SessionProvider>
   )
 }
 
