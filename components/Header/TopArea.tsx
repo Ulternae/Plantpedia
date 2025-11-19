727ad7ca27cfbbbeb8b0d2fa6b5a033b5809320f
Ulternae <ulternae@gmail.com>
2025-11-19 10:19:22 -0500

    Login Logout

--- components/Header/TopArea.tsx
+++ components/Header/TopArea.tsx
@@ -3,8 +3,46 @@ import { useRouter } from 'next/router'
 import { Grid } from '@ui/Grid'
 import { Typography } from '@ui/Typography'
 import { Button } from '@ui/Button'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function TopArea() {
+
+
+  return (
+    <Grid container justify="space-between">
+      <Grid item>
+        <LoginLogout />
+      </Grid>
+      <Grid item>
+        <LocaleOptions />
+      </Grid>
+    </Grid>
+  )
+}
+
+export const LoginLogout = () => {
+  const [session, loading] = useSession()
+
+  if (loading) {
+    return null
+    // return <Typography>Loading...</Typography>
+  }
+  if (session === null) {
+    return (
+      <Button variant="outlined" onClick={() => signIn()}>
+        Login
+      </Button>
+    )
+  }
+
+  return (
+    <Button variant="outlined" onClick={() => signOut()}>
+      {session?.user?.name} Logout
+    </Button>
+  )
+}
+
+export const LocaleOptions = () => {
   const { locales, locale } = useRouter()
 
   // Locales aren't configured
@@ -13,27 +51,24 @@ export function TopArea() {
   }
 
   return (
-    <Grid container justify="space-between">
-      <Grid item></Grid>
-      <Grid item>
-        {locales.map((loc) => (
-          <form
-            action="/api/language"
-            method="POST"
-            key={loc}
-            className="inline-block"
+    <>
+      {locales.map((loc) => (
+        <form
+          action="/api/language"
+          method="POST"
+          key={loc}
+          className="inline-block"
+        >
+          <input name="preferredLocale" value={loc} type="hidden"></input>
+          <Button
+            variant={loc === locale ? 'outlined' : 'text'}
+            className="ml-1"
+            type="submit"
           >
-            <input name="preferredLocale" value={loc} type="hidden"></input>
-            <Button
-              variant={loc === locale ? 'outlined' : 'text'}
-              className="ml-1"
-              type="submit"
-            >
-              {loc}
-            </Button>
-          </form>
-        ))}
-      </Grid>
-    </Grid>
+            {loc}
+          </Button>
+        </form>
+      ))}
+    </>
   )
 }
